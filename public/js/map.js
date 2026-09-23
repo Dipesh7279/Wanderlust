ad552083c450d8a4c8d3ef72bdb07a442a6f82e6
Harsh Agrawal <harsh.agrawal123@gmail.com>
2026-09-23 18:17:07 +0530

    corrected location

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,7 @@
 
     
     const map = new mapboxgl.Map({
-        accessToken: 'mapToken',
+        accessToken: mapToken,
         container: 'map', // container ID
         center: [79.0889,21.1466 ], // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
